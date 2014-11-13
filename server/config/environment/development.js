25b0600b9a76acc738b99e75b80ce0b2d5e5c6f3
xwang17 <xwang17@ebay.com>
2014-11-13 12:39:27 -0800

    change DB

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,17 +5,17 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://10.9.241.212/comsmonitoring-dev'
+    uri: 'mongodb://10.9.210.30/hackathon'
   },
 
   seedDB: true,
 
   mysql: {
-  	host: '10.9.241.202',
+  	host: '10.9.210.31',
   	port: 3306,
   	user: 'root',
-  	password: 'gvMw7TTcnRsTyyGxDedVwcKFd3RFds2V3WPA',
-  	database: 'coms_monitoring_dev',
+  	password: 'HFc9Vbx6uaZDAybsxKKjsaQYGcc2cpmNsskv',
+  	database: 'hackathon',
   	connectionLimit: 3
   }
 };
